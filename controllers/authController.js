4936a700307472c929780dafc5936a7cb1f69dea
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-10-31 16:52:01 +0100

    httpOnly set false

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    // httpOnly: true
   };
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
@@ -69,7 +69,7 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.logout = (req,res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
+    // httpOnly: true
   };
   res.cookie('jwt', '', cookieOptions)
   res.status(200).json({ status: 'success' })

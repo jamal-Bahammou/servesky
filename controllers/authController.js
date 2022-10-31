a0fff5d5249365f1059c70b719a185cfbbbe8c62
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-10-31 17:19:12 +0100

    ADD DOMAIN SUB TO COOKIE

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,9 +18,10 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    domain: ".vercel.app",
   };
-  
+
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
@@ -70,7 +71,8 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.logout = (req,res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
+    httpOnly: true,
+    domain: ".vercel.app",
   };
   res.cookie('jwt', '', cookieOptions)
   res.status(200).json({ status: 'success' })

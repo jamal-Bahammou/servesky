aa46cb847fa3962e4fba20d872b31a7e23cccdc8
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-11-03 14:15:53 +0100

    DEFAULT PROFIL IMAGE

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,6 +38,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
+    photo: "/img/users/default.jpg",
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });

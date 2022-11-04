4556f03fe5de1501c009c2b72bac02ae88aca40c
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-11-04 15:09:58 +0100

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true,
+    // secure: true,
     sameSite: 'None',
   };
 
@@ -72,7 +72,7 @@ exports.logout = (req,res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
-    secure: true,
+    // secure: true,
     sameSite: 'None',
   };
   res.cookie('jwt', '', cookieOptions)
